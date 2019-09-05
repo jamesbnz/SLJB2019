a3a3144f91b33e36a402774f2f12e511e7c8c360
James <j.beuvink01@gmail.com>
2019-09-05 12:39:23 +1200

    skybox

---
+++ Assets/RailTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class RailTest : MonoBehaviour {
+    public Vector3[] points = new Vector3[0];
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+
+        for (int i = 0; i < points.Length; i++) {
+            Gizmos.DrawSphere(points[i], 1f);
+
+            if (i > 0) {
+                Gizmos.DrawLine(points[i - 1], points[i]);
+            }
+        }
+    }
+
+}
