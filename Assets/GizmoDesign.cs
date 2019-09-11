1efb7be00ce215211735d0721edd68e5453c5d94
James <j.beuvink01@gmail.com>
2019-09-11 12:21:06 +1200

    new lifts

---
+++ Assets/GizmoDesign.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GizmoDesign : MonoBehaviour {
+    private void OnDrawGizmos() {
+        Gizmos.color = new Color(1, 0, 0);
+        Vector3 start = transform.position - (transform.forward * transform.localScale.magnitude);
+        Vector3 end = transform.position + (transform.forward * transform.localScale.magnitude);
+        
+        Gizmos.DrawLine(
+            start
+        ,   end
+        );
+
+        Handles.DrawAAPolyLine(20, new Vector3[] {
+            start,
+            end
+        });
+    }
+}
