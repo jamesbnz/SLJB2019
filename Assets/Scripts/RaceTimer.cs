8cb89ed0ee06735c40df4db3b66680940b4100d9
James <j.beuvink01@gmail.com>
2019-11-20 22:09:23 +1300

    timer terrain

---
+++ Assets/Scripts/RaceTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaceTimer : MonoBehaviour
+{
+    public Text timerText;
+    public Canvas raceCanvas;
+    public Rigidbody player;
+    public Collider startBox;
+    public Collider finishBox;
+    bool playing;
+    float theTime;
+
+    void Update()
+    {
+            theTime += Time.deltaTime;
+            string minutes = Mathf.Floor((theTime % 3600) / 60).ToString("00");
+            string seconds = (theTime % 60).ToString("00");
+            timerText.text = minutes + ":" + seconds;
+    }
+}
+
+
---
+++ Assets/StartBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBox : MonoBehaviour
+{
+    public Rigidbody player;
+    public GameObject racetimer;
+
+    private void OnTriggerEnter(Collider trigger)
+    {
+        racetimer.SetActive(true); 
+    }
+}
